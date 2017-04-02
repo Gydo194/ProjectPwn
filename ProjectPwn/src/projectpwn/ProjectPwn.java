20c77ec6f6a2a5974184263ecc2d29030c7a4015
gydokosten <gydokosten@192.168.1.104>
2017-04-02 13:52:53 +0200

    Project Pwn built-in server, v1.0 bug fixes

--- ProjectPwn/src/projectpwn/ProjectPwn.java
+++ ProjectPwn/src/projectpwn/ProjectPwn.java
@@ -22,31 +22,46 @@ public class ProjectPwn {
     /**
      * @param args the command line arguments
      */
+   
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        //Start the HTTP Server core
         HttpServer projectServer = new HttpServer();
-            Thread projectServerThread = new Thread(projectServer);
-            projectServerThread.start(); //starts the server in new thread
-            System.out.println("Starting httpserver.setup()");
-            System.out.println("Starting httpserver.addURL()");
-            projectServer.addURL("/ack","Acknowledgement complete");
-            projectServer.addURL("/dist/projectpwn", "<html><h1>Welcome to ProjectPwn</h1><br><p>This is a demo page for the built-in web server.</p></html>");
-            projectServer.addURL("/asdf", "LEL, asdf. haha.");
-            if(projectServer.urlExists("/asdf")){
-               System.out.println("Removing Url"); //removes the URL only if it exists, to create a 404 again
-                projectServer.removeUrl("/asdf");
-            }
-            projectServer.addURL("/threadid", "HTTP Server current threadid: " + Long.toString(projectServerThread.getId()) );
-            //test adding URLS
-            //JFrame addFrame = new JFrame();
-            //String UrlToAdd = JOptionPane.showInputDialog(null,"Add new URL to the server");
-            //String DataToAdd = JOptionPane.showInputDialog(null,"Add return data for URL " + UrlToAdd);
-            //projectServer.addURL(UrlToAdd,DataToAdd);
-            projectServer.printArrays();
+        Thread projectServerThread = new Thread(projectServer);
+        projectServerThread.start(); //starts the server in new thread
+            
+        //start the GUI   
+        Gui projectGui = new Gui();
+        projectGui.setVisible(true);
+        projectGui.DatabaseLabel.setText("set from code"); //sets the database label text
+        projectGui.ExecuteCustomCodeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                if(projectServer.urlExists("/event")){
+                projectServer.removeUrl("/event");
+                }
+                projectServer.addURL("/event", projectGui.CustomCodeTextArea.getText());
+                }
+            });
+            
+            
+            
+        projectServer.addURL("/ack","Acknowledgement complete");
+        projectServer.addURL("/dist/projectpwn", "<html><h1>Welcome to ProjectPwn</h1><br><p>This is a demo page for the built-in web server.</p></html>");
+        projectServer.addURL("/asdf", "LEL, asdf. haha.");
+        if(projectServer.urlExists("/asdf")){
+            System.out.println("Removing Url"); //removes the URL only if it exists, to create a 404 again
+            projectServer.removeUrl("/asdf");
+        }
+        projectServer.addURL("/threadid", "HTTP Server current threadid: " + Long.toString(projectServerThread.getId()) );
+        projectServer.addURL("/404", "http 200 ok.");
+        projectServer.printArrays();
             
             
         
         
     }
+    
     
 }
