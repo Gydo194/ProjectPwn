20ae7b9bb6351bce7019cb9e2c92ee704484fa2e
gydokosten <gydokosten@192.168.1.104>
2017-04-02 08:56:20 +0200

    Web server and its API v1.0, now running in separate thread.

--- ProjectPwn/src/projectpwn/ProjectPwn.java
+++ ProjectPwn/src/projectpwn/ProjectPwn.java
@@ -19,10 +19,20 @@ public class ProjectPwn {
     public static void main(String[] args) {
         // TODO code application logic here
         HttpServer projectServer = new HttpServer();
+            Thread projectServerThread = new Thread(projectServer);
+            projectServerThread.start(); //starts the server in new thread
             System.out.println("Starting httpserver.setup()");
             System.out.println("Starting httpserver.addURL()");
             projectServer.addURL("/ack","Acknowledgement complete");
-            projectServer.run();
+            projectServer.addURL("/dist/projectpwn", "<html><h1>Welcome to ProjectPwn</h1><br><p>This is a demo page for the built-in web server.</p></html>");
+            projectServer.addURL("/asdf", "LEL, asdf. haha.");
+            if(projectServer.urlExists("/asdf")){
+                System.out.println("Removing Url"); //removes the URL only is it exists, to create a 404 again
+                projectServer.removeUrl("/asdf");
+            }
+            projectServer.addURL("/threadid", "HTTP Server current threadid: " + Long.toString(projectServerThread.getId()) );
+            
+            
         
         
     }
